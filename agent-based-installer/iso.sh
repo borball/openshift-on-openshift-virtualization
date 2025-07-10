a0bdef561d1328f021e62635c1e81d89fcbd608d
Borball Zhai <borball.zh@gmail.com>
2025-07-10 14:07:22 -0400

    fix iso

--- agent-based-installer/iso.sh
+++ agent-based-installer/iso.sh
@@ -3,6 +3,6 @@
 git clone git@github.com:borball/mno-with-abi.git
 cp vacm1-config.yaml mno-with-abi/vacm1-config.yaml
 cd mno-with-abi
-./mno-iso.sh vacm1-config.yaml
+./mno-iso.sh vacm1-config.yaml stable-4.18
 
 cp instances/vacm1/agent.x86_64.iso /var/www/html/iso/vacm1.iso
