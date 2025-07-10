324e84c4a43b937a7a7d03e9a934f58b14c81bcc
Borball Zhai <borball.zh@gmail.com>
2025-07-10 14:41:34 -0400

    add iso in cdrom

--- agent-based-installer/iso.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf mno-with-abi
-git clone git@github.com:borball/mno-with-abi.git
-cp vacm1-config.yaml mno-with-abi/vacm1-config.yaml
-cd mno-with-abi
-./mno-iso.sh vacm1-config.yaml stable-4.18
-
-cp instances/vacm1/agent.x86_64.iso /var/www/html/iso/vacm1.iso
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+create_iso(){
+    rm -rf mno-with-abi
+    git clone git@github.com:borball/mno-with-abi.git
+    cp vacm1-config.yaml mno-with-abi/vacm1-config.yaml
+    cd mno-with-abi
+    ./mno-iso.sh vacm1-config.yaml stable-4.18
+
+    cp instances/vacm1/agent.x86_64.iso /var/www/html/iso/vacm1.iso
+}
+
+create_localnet(){
+    oc apply -f virtual-machines/localnet.yaml
+}
+
+creare-acm-hub1-vms(){
+    oc apply -k virtual-machines/acm-hub1
+}
+
+power_on_acm-hub1-vms(){
+}
+
+power_off_acm-hub1-vms(){
+}
+
+mount_iso_to_acm-hub1-vms(){
+
+}
+
+unmount_iso_from_acm-hub1-vms(){
+}
+
